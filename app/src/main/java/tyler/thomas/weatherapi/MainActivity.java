2ea16e2e007119f7f98b168337169aeaf1f6c653
Tyler <n01344909@humbermail.ca>
2021-05-29 05:55:14 -0400

    designing the layout finished

--- app/src/main/java/tyler/thomas/weatherapi/MainActivity.java
+++ app/src/main/java/tyler/thomas/weatherapi/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.sql.DatabaseMetaData;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
