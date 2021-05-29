46e1669017d20e547e57d6bc2538757a6a24861e
Tyler <n01344909@humbermail.ca>
2021-05-29 06:10:54 -0400

    setup some button listeners

--- app/src/main/java/tyler/thomas/weatherapi/MainActivity.java
+++ app/src/main/java/tyler/thomas/weatherapi/MainActivity.java
@@ -3,17 +3,48 @@ package tyler.thomas.weatherapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import java.sql.DatabaseMetaData;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+Button btn_CityID;
+Button btn_getWeatherByID;
+Button btn_getWeatherByName;
+EditText et_dataInput;
+ListView lv_weatherReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        //Assigning values to each control in the layout.
+        btn_CityID = findViewById(R.id.btn_getCityId);
+        btn_getWeatherByID = findViewById(R.id.btn_getWeatherByCityID);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+        et_dataInput = findViewById(R.id.et_dataInput);
+        lv_weatherReport = findViewById(R.id.lv_weatherReports);
+        //Click listeners for each button.
+        btn_CityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Hello",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Hello2",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this," Three",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
