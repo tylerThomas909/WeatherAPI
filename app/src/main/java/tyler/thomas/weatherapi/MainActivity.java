42906503e413980f1b2693407339749e71112516
Tyler <n01344909@humbermail.ca>
2021-05-30 11:00:46 -0400

    changed up the code still getting the same error trying to solve it

--- app/src/main/java/tyler/thomas/weatherapi/MainActivity.java
+++ app/src/main/java/tyler/thomas/weatherapi/MainActivity.java
@@ -9,6 +9,18 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 Button btn_CityID;
 Button btn_getWeatherByID;
@@ -29,8 +41,29 @@ ListView lv_weatherReport;
         //Click listeners for each button.
         btn_CityID.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"Hello",Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {//Semantic error
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url ="https://www.metaweather.com/api/location/search/?query=london";
+                JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET,url,null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        String cityID = "";
+                        try {
+                            JSONObject cityinfo = response.getJSONObject(0);
+                             cityID = cityinfo.getString("woeid");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Toast.makeText(MainActivity.this,"City ID" + cityID,Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,"Error!",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                queue.add(request);
             }
         });
         btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
